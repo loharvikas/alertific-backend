d086157289d6d7ae9129e6d2fc663f4277044c6a
loharvikas <loharvikas13@gmail.com>
2021-08-26 10:00:35 +0530

    DEBUG=FALSE

--- alertific/settings.py
+++ alertific/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "sfoihwifwegfwhfjbfo2bfi2gf2fkjwblisbvviusfwfwejfhiwfw")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
